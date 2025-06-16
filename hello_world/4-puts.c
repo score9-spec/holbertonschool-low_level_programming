82effa8eac3a277fd5bcece90c315d8dd0d401e5
score9-spec <alfreidine@gmail.com>
2025-06-16 20:01:50 +0000

    Add 4-puts.c: Print string using puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Description: Prints a string using puts function
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
