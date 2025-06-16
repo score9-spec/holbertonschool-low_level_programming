23c1f854beee42a03facabef0a81736331556a3c
score9-spec <alfreidine@gmail.com>
2025-06-16 19:47:47 +0000

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
+ * Description: Prints a string using puts function
  * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0; // Retiré les parenthèses autour de 0
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
