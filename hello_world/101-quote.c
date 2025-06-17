55093f30d1f04835a638f007a5b2eaa3087943c9
score9-spec <alfreidine@gmail.com>
2025-06-17 13:15:27 +0000

    fix 101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,15 +1,15 @@
-#include <unistd.h>    
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
  * Return: 1
  */
-int main(void)    
+int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 
-	write(2,quote, 59);
-	return (1);    
+	write(2, quote, 59);
+	return (1);
 }
