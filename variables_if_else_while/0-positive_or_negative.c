192fb635337ef06ee7402d60c2dc78a0f8996249
score9-spec <alfreidine@gmail.com>
2025-06-18 09:29:09 +0000

    Fixed Betty style issues in 0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+	* main - Entry point of the program
+	*        prints wether a random number is positive, zero or negative
+	*
+	*Return: Always 0 (Success)
+	*/
+int main(void)
+{
+	int n;
+
+
+	srand(time(NULL));
+
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
