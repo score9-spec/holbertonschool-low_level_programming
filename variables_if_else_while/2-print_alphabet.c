0369f83df48075300cbe01aeea5cbec981d57183
score9-spec <alfreidine@gmail.com>
2025-06-18 10:21:12 +0000

    fix code

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-char c;
+	char c;
 
-for (c = 'a'; c <= 'z'; c++)
-{
-putchar(c);
-}
-putchar('\n');
-return (0);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
