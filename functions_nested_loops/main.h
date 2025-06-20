ebf137465d8cb5388cfe19d350366bb118534abb
score9-spec <alfreidine@gmail.com>
2025-06-20 10:32:26 +0000

    update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/*
+* File: main.h
+* Desc: Header file containing prototypes for all functions
+*       included in the Holberton School C programming project.
+*/
+
+#include <unistd.h>  /* Inclut write pour _putchar (même si _putchar est souvent dans main.h, write vient de unistd.h) */
+
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif /* MAIN_H */
