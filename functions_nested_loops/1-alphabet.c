bf18c39a78b115ea2da0d38068e37f95fef0b4c4
score9-spec <alfreidine@gmail.com>
2025-06-20 10:31:05 +0000

    correction des erreur de compilation pour 1-alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
- */
+* print_alphabet - prints the lowercase alphabet, followed by a new line
+*/
 void print_alphabet(void)
 {
-	char c;
+char letter;
 
-	for (c = 'a' ; c <= 'z' ; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n'); /* Ajoutez un saut de ligne a la fin */
 }
