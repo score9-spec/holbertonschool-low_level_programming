650b8ce9ad06a4bf76e49af821aaf17f3da933f5
score9-spec <alfreidine@gmail.com>
2025-06-21 11:00:06 +0000

    fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,46 +1,51 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: the size of the times table to print
- */
+* print_times_table - prints the n times table, starting with 0
+* @n: the size of the times table to print
+*/
 void print_times_table(int n)
 {
-    int i, j, product;
+int i, j, product;
 
-    if (n < 0 || n > 15)
-        return;
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            product = i * j;
-            if (j == 0)
-                _putchar('0' + product);
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                if (product < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar('0' + product);
-                }
-                else if (product < 100)
-                {
-                    _putchar(' ');
-                    _putchar('0' + (product / 10));
-                    _putchar('0' + (product % 10));
-                }
-                else
-                {
-                    _putchar('0' + (product / 100));
-                    _putchar('0' + ((product / 10) % 10));
-                    _putchar('0' + (product % 10));
-                }
-            }
-        }
-        _putchar('\n');
-    }
+if (n < 0 || n > 15)
+return;
+
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+product = i * j;
+
+if (j == 0)
+{
+_putchar('0' + product);
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+
+if (product < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar('0' + product);
+}
+else if (product < 100)
+{
+_putchar(' ');
+_putchar('0' + (product / 10));
+_putchar('0' + (product % 10));
+}
+else
+{
+_putchar('0' + (product / 100));
+_putchar('0' + ((product / 10) % 10));
+_putchar('0' + (product % 10));
+}
+}
+}
+_putchar('\n');
+}
 }
