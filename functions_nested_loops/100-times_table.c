b1acb5e831d596fcb0a3c74eceb78c0094a7017c
score9-spec <alfreidine@gmail.com>
2025-06-21 14:03:36 +0000

    fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -9,7 +9,6 @@ void print_times_table(int n)
 int i, j;
 
 if (n < 0 || n > 15)
-
 return;
 
 for (i = 0; i <= n; i++)
@@ -17,13 +16,15 @@ for (i = 0; i <= n; i++)
 for (j = 0; j <= n; j++)
 {
 int prod = i * j;
-if (j)
+
+if (j != 0)
+{
 _putchar(',');
-if (j)
 _putchar(' ');
-if (prod < 10 && j && n >= 10)
+}
+if (prod < 10 && j != 0)
 _putchar(' ');
-if (prod < 100 && n >= 10 && j)
+if (prod < 100 && j != 0 && n >= 10)
 _putchar(' ');
 if (prod >= 100)
 _putchar(prod / 100 + '0');
