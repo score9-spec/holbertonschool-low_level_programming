1f7b278cc8310fbff6ef6c850cd710fc61f87ddb
score9-spec <alfreidine@gmail.com>
2025-06-21 18:29:03 +0000

    fix

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,31 @@
 #include <stdio.h>
 
 /**
-* print_fibonacci - Imprime les 98 premiers nombres de Fibonacci.
-*/
-void print_fibonacci(void)
-{
-unsigned int a_low = 1;
-unsigned int b_low = 2;
-unsigned int temp_low;
-unsigned int a_high = 0;
-unsigned int b_high = 0;
-unsigned int temp_high;
-int i;
-
-printf("%u, %u, ", a_low, b_low);
-for (i = 2; i < 98; i++)
-{
-temp_low = a_low + b_low;
-temp_high = a_high + b_high + (temp_low < a_low);
-a_low = b_low;
-a_high = b_high;
-b_low = temp_low;
-b_high = temp_high;
-
-if (b_high == 0)
-printf("%u, ", b_low);
-else
-printf("%u%09u, ", b_high, b_low);
-}
-printf("\n");
-}
-
-/**
-* main - Fonction principale.
+* main - Entry point
 *
-* Return: Toujours 0.
- */
+* This program prints the first 98 Fibonacci numbers,
+* starting with 1 and 2, separated by a comma and a space.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-print_fibonacci();
+unsigned long int a = 1, b = 2, next;
+int i;
+
+/* Print the first two Fibonacci numbers */
+printf("%lu, %lu", a, b);
+
+/* Generate and print the next 96 Fibonacci numbers */
+for (i = 2; i < 98; i++)
+{
+next = a + b;
+printf(", %lu", next);
+a = b;
+b = next;
+}
+
+printf("\n");
+
 return (0);
 }
