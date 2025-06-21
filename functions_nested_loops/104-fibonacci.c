f7c4fd0f8c55703d0617baec1cbd6b5174541f55
score9-spec <alfreidine@gmail.com>
2025-06-21 16:06:31 +0000

    add code  104-fibonacci.c

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,31 @@
 */
 int main(void)
 {
-unsigned long int aLow = 1, aHigh = 0;
-unsigned long int bLow = 2, bHigh = 0;
-unsigned long int nextLow, nextHigh;
+unsigned long int a1 = 1;
+unsigned long int a2 = 0;
+unsigned long int b1 = 2;
+unsigned long int b2 = 0;
 int i;
 
-printf("%lu, %lu, ", aLow, bLow);
+printf("%lu, %lu, ", a1, b1);
 
 for (i = 2; i < 98; i++)
 {
-nextLow = aLow + bLow;
-nextHigh = aHigh + bHigh + (nextLow < aLow);
-printf("%lu", nextHigh * 1000000000 + nextLow);
+unsigned long int next1 = a1 + b1;
+unsigned long int next2 = a2 + b2 + (next1 < a1);
+
+if (next2)
+printf("%lu%09lu", next2, next1);
+else
+printf("%lu", next1);
+
 if (i != 97)
 printf(", ");
-aLow = bLow;
-aHigh = bHigh;
-bLow = nextLow;
-bHigh = nextHigh;
+
+a1 = b1;
+a2 = b2;
+b1 = next1;
+b2 = next2;
 }
 
 printf("\n");
