a8238c58163a69ab2de2f429dc75f4482e97fb08
score9-spec <alfreidine@gmail.com>
2025-06-23 14:47:30 +0000

    add 101-print_numbers

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,20 @@
 #include "main.h"
+#include <limits.h> /* Inclure limits.h pour utiliser INT_MIN */
 
-/**
-* print_number - imprime un entier
-* @n: l'entier à imprimer
-*/
 void print_number(int n)
 {
 if (n < 0)
 {
 _putchar('-');
+if (n == INT_MIN)
+{
+_putchar('2');
+n = -(n + 2);
+}
+else
+{
 n = -n;
+}
 }
 if (n / 10 != 0)
 {
