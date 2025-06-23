144603a4754482d3169e67b1824e40079d45d6e4
score9-spec <alfreidine@gmail.com>
2025-06-23 17:50:16 +0000

    add task 3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,21 @@ void swap_int(int *a, int *b);
  */
 int _strlen(char *s);
 
+/**
+ * _puts - prints a string to stdout
+ * @str: string to print
+ *
+ * Return: void
+ */
+void _puts(char *str);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
 #endif /* MAIN_H */
