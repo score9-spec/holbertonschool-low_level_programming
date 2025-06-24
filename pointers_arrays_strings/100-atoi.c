a1804aea04f28950e513b54f91ab81e3bce32cdd
score9-spec <alfreidine@gmail.com>
2025-06-24 14:55:18 +0000

    add Task 10 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-* _atoi - convert a string to an integer
-* @s: the input string
+* _atoi - convertit une chaîne en entier
+* @s: chaîne d’entrée
 *
 * Description:
-*   - Parcourt tous les caractères jusqu’au premier chiffre.
-*   - Inverse le signe à chaque '-' rencontré.
-*   - Une fois un chiffre trouvé, accumule la valeur en
-*     tenant compte du signe à chaque itération.
-*   - Si aucun chiffre n’est trouvé, renvoie 0.
+*   - Parcourt tous les caractères jusqu’au premier chiffre
+*     (flip du signe à chaque '-' rencontré).
+*   - Accumule ensuite chaque caractère chiffre en appliquant
+*     immédiatement le signe pour éviter l’overflow sur INT_MIN.
+*   - Si aucun chiffre n’est rencontré, renvoie 0.
 *
-* Return: la valeur entière
+* Return: valeur entière convertie
 */
 int _atoi(char *s)
 {
@@ -19,24 +19,23 @@ int i = 0;
 int sign = 1;
 int result = 0;
 
-/* 1) Parcours jusqu’à la première occurrence de '0'..'9', track '-' */
+/* 1) skippage jusqu’au premier digit, flip sign sur '-' */
 while (s[i] && (s[i] < '0' || s[i] > '9'))
 {
 if (s[i] == '-')
 sign = -sign;
-/* on ignore '+' */
 i++;
 }
 
-/* 2) Accumulation chiffre par chiffre, on applique le signe immédiatement */
+/* 2) accumulation chiffre par chiffre */
 while (s[i] >= '0' && s[i] <= '9')
 {
-if (sign > 0)
+if (sign == 1)
 result = result * 10 + (s[i] - '0');
 else
 result = result * 10 - (s[i] - '0');
 i++;
 }
 
-return result;
+return (result);
 }
