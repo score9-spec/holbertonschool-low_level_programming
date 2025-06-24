a924b12eb1b8e555162d758eb49993ed37eaf2c9
score9-spec <alfreidine@gmail.com>
2025-06-24 13:36:25 +0000

    add task 9 9-strcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -84,4 +84,13 @@ void puts_half(char *str);
  */
 void print_array(int *a, int n);
 
+/**
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: the destination buffer
+ * @src: the source string
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src);
+
 #endif /* MAIN_H */
