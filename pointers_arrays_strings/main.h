ce34e77998fd1bb18f607268e6bd2aa970c86869
score9-spec <alfreidine@gmail.com>
2025-06-24 13:25:53 +0000

    add Task 8 / 8-print_array

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -75,4 +75,13 @@ void puts2(char *str);
  */
 void puts_half(char *str);
 
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: the array to be printed
+ * @n: the number of elements to be printed
+ *
+ * Return: void
+ */
+void print_array(int *a, int n);
+
 #endif /* MAIN_H */
