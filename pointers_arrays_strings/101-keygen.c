68972e25c0adc03e8218dc2c9c8c49c875fef972
score9-spec <alfreidine@gmail.com>
2025-06-24 17:29:38 +0000

    add task 11 101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
-* main - Generates a random valid password for 101-crackme
+* main - Generates a random password
 *
 * Return: Always 0 (Success)
 */
@@ -12,23 +12,14 @@ int main(void)
 int i;
 char password[7];
 
-srand(time(NULL)); /* seed the random number generator */
+srand(time(NULL));
 
-/* Generate a random password */
-password[0] = (rand() % 26) + 'A'; /* first character is an uppercase letter */
-for (i = 1; i < 6; i++)
+for (i = 0; i < 7; i++)
 {
-password[i] = (rand() % 62); /* generate a random character */
-if (password[i] < 10) /* if it's a digit, add '0' */
-password[i] += '0';
-else if (password[i] < 36) /* if it's an uppercase letter, add 'A' - 10 */
-password[i] += 'A' - 10;
-else /* if it's a lowercase letter, add 'a' - 36 */
-password[i] += 'a' - 36;
+password[i] = (rand() % 26) + 'A';
 }
-password[6] = '\0'; /* null-terminate the string */
 
 printf("%s\n", password);
 
-return (0);
+return 0;
 }
