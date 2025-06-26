f48719244b9cae4d4f7c157932afcb12149fc930
score9-spec <alfreidine@gmail.com>
2025-06-26 10:33:11 +0000

    Ajout de la fonction _strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
-* _strcat - Concat√ne deux chaines de caracteres.
+* _strcat - Concat√®ne deux chaines de caracteres.
 * @dest: La chaine de caracteres a destination.
-* @src: La chaine de caract√res a concatener.
+* @src: La chaine de caracteres a concatener.
 *
-* Return: Un pointeur vers la chaine r√sultante.
+* Return: Un pointeur vers la chaine r√©sultante.
 */
 char *_strcat(char *dest, char *src)
 {
 int i = 0;
 int j = 0;
 
-/* trouver la fin de la chaine dest */
 while (dest[i] != '\0')
-{
 i++;
-}
 
-/* Concat√ner src a la fin de dest */
 while (src[j] != '\0')
 {
 dest[i] = src[j];
@@ -26,7 +22,6 @@ i++;
 j++;
 }
 
-/* Ajouter un caract√re null a la fin */
 dest[i] = '\0';
 return (dest);
 }
